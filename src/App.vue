661699d3fb24f5ea0e25d9e7a7951c28f06cd099
Marcin Wolak <marcin.wolak@ocado.com>
2020-05-26 14:00:32 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-content>
+        <v-select
+        :value="selectedFruits"
+        :items="fruits"
+        label="Favorite Fruits"
+        multiple
+        @blur="onBlur"
+        >
+        <template v-slot:prepend-item>
+            <v-list-item
+            ripple
+            @click="toggle"
+            >
+            <v-list-item-content>
+                <v-list-item-title>Select All</v-list-item-title>
+            </v-list-item-content>
+            </v-list-item>
+        </template>
+        </v-select>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data: () => ({
+      fruits: [
+        'Apples',
+        'Apricots',
+      ],
+      selectedFruits: [],
+    }),
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    computed: {
+      likesAllFruit () {
+        return this.selectedFruits.length === this.fruits.length
+      },
+    },
+    methods: {
+        onBlur() {
+            console.log("Blur")
+        },
+      toggle () {
+        this.$nextTick(() => {
+          if (this.likesAllFruit) {
+            this.selectedFruits = []
+          } else {
+            this.selectedFruits = this.fruits.slice()
+          }
+        })
+      },
+    },
+};
+</script>
